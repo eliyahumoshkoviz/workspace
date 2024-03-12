89f380e5925b79ef89f4f27c6618c142f43c6bf0
eliyahumoshkoviz <054epm@gmail.com>
2024-03-12 18:13:19 +0200

    update details

--- clientSide/src/component/Task.jsx
+++ clientSide/src/component/Task.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 
 function Task({ title, description, assignedTo, status }) {
     return (
-        <div className="w-full p-4 bg-white border rounded-lg shadow-md ">
-            <h3 className="mb-2 text-xl font-semibold">{title}</h3>
-            <p className="text-gray-600">{description}</p>
-            <p className="text-gray-600">{assignedTo}</p>
-            <p className="text-gray-600">{status}</p>
+        <div className="w-full p-4 bg-white border rounded-lg shadow-md">
+        <h3 className="mb-2 text-xl font-semibold">{title}</h3>
+        <div className="flex flex-wrap items-center mb-2">
+            <span className="mr-2 text-gray-600">Description:</span>
+            <p className="text-gray-800">{description}</p>
         </div>
+        <div className="flex flex-wrap items-center mb-2">
+            <span className="mr-2 text-gray-600">Assigned to:</span>
+            <p className="text-gray-800">{assignedTo}</p>
+        </div>
+        <div className="flex flex-wrap items-center mb-2">
+            <span className="mr-2 text-gray-600">Status:</span>
+            <p className="text-gray-800">{status}</p>
+        </div>
+    </div>
+    
     )
 }
 
